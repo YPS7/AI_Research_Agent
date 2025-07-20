33a69cf83d1b8cf2a15d6019aaaa69fbc57ebb40
Yash Pratap Singh <72648146+YPS7@users.noreply.github.com>
2025-07-20 13:35:46 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,17 +5,7 @@ from langchain_community.tools import DuckDuckGoSearchRun, WikipediaQueryRun
 from langchain.tools import Tool
 from langchain.tools import DuckDuckGoSearchRun, Tool
 
-# search = DuckDuckGoSearchRun()
-# search_tool  =Tool(
-#     name="search",
-#     func=search.run,
-#     description="Search the web for information",
-# )
-
-# Instantiate the DuckDuckGo “run” tool
 search_runner = DuckDuckGoSearchRun()
-
-# Wrap it in a LangChain Tool
 search_tool = Tool(
     name="web_search",
     func=search_runner.run,
